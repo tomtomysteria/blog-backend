9398ab6f486f2c190aba00202eb0bc31283284ef
Thomas CESBRON <thomas.cesbron@avec.fr>
2024-08-28 11:41:44 +0200

    feat(app): create shared module to handle JwtModule globally in the app

---
+++ src/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { Global, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Global()
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET') || 'secretKey',
+        signOptions: { expiresIn: '60m' },
+      }),
+    }),
+  ],
+  exports: [JwtModule],
+})
+export class SharedModule {}
