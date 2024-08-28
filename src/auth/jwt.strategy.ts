61040f95537752d6aed9ae1d704f3432d9093a08
Thomas CESBRON <thomas.cesbron@avec.fr>
2024-08-28 12:21:13 +0200

    feat(auth): configure JwtStrategy with ConfigService

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,15 +1,19 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly usersService: UsersService) {
+  constructor(
+    private readonly usersService: UsersService,
+    configService: ConfigService, // Ne pas attribuer à une propriété de la classe pour éviter de déclarer une propriété inutilisée dans la classe
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET || 'secretKey',
+      secretOrKey: configService.get<string>('JWT_SECRET') || 'secretKey', // Utiliser configService directement
     });
   }
 
