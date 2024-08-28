17e45ac3367ff8602cd95d2c3dc1599a43e2aa83
Thomas CESBRON <thomas.cesbron@avec.fr>
2024-08-28 11:52:34 +0200

    feat(auth): improve AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
-import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'secretKey',
-      signOptions: { expiresIn: '60m' },
-    }),
-  ],
+  imports: [forwardRef(() => UsersModule), SharedModule],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
