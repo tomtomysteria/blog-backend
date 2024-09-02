e834fd03f86ceff314a05e2b10028fff31864f45
Thomas CESBRON <thomas.cesbron@avec.fr>
2024-09-02 20:40:18 +0200

    feat(roles): configure @UseGuards and @Roles decorators to handle role in categorie controller

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -13,6 +13,8 @@ import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
 import { Request } from 'express';
 import { User } from '../users/user.entity';
 
@@ -21,7 +23,8 @@ export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('super-admin', 'admin')
   create(@Body() createCategoryDto: CreateCategoryDto, @Req() req: Request) {
     const user = req.user as User;
     return this.categoriesService.create(createCategoryDto, user);
@@ -38,7 +41,8 @@ export class CategoriesController {
   }
 
   @Put(':id')
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('super-admin', 'admin')
   update(
     @Param('id') id: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
@@ -49,7 +53,8 @@ export class CategoriesController {
   }
 
   @Delete(':id')
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('super-admin', 'admin')
   delete(@Param('id') id: string) {
     return this.categoriesService.delete(id);
   }
