766a2003339f663c27fd18358e978273174c8d7e
Thomas CESBRON <thomas.cesbron@avec.fr>
2024-09-23 16:49:38 +0200

    config(app): config default port

--- src/main.ts
+++ src/main.ts
@@ -39,7 +39,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = configService.get<number>('PORT', 3000);
+  const port = configService.get<number>('PORT', 4000);
   await app.listen(port);
 }
 bootstrap();
